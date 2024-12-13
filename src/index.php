d48ef788f6e7557b91d93b2dbaaf98ece2e7373e
kkard2 <contact@kkard2.com>
2024-12-13 20:08:42 +0100

    i need to think first

--- src/index.php
+++ src/index.php
@@ -1,3 +1,23 @@
-
-<?php echo 'hello world' ?>
-<a href="/test.html">test</a>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>##title##</title>
+        <link rel="stylesheet" href="/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header>
+            <a href="/" class="filesystem-root">/</a>
+            <a href="/software">/software</a>
+            <a href="/blog">/blog</a>
+            <a href="/rss">/rss</a>
+            <a href="/people">/people</a>
+            <a href="/ophs">/ophs</a>
+        </header>
+        <div>
+            <h1>test h1</h1>
+            <h2>test h2</h2>
+            <h3>test h3</h3>
+        </div>
+    </body>
+</html>
